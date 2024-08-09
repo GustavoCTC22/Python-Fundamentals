43c21d7ebbe1651a1831b586deb5f29d1d08e313
GustavoUT22 <gustavougartetorres@gmail.com>
2024-08-08 21:28:02 -0500

    primer aproach de clases en python

---
+++ 02-intermediate/08-classes.py
@@ -0,0 +1,17 @@
+# sintaxis
+class Animal:
+    def __init__(self, nombre, peso):
+        self.nombre = nombre
+        self.peso = peso
+
+    def comer(self):
+        print(f"{self.nombre} está comiendo ")
+
+    def dormir():
+        print("El animal está durmiendo")
+
+
+gato = Animal("michi", "5kg")
+print(gato.nombre)
+print(gato.peso)
+gato.comer()
