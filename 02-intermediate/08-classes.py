83827d32dad3f4a4208ab6f463d92e015814f065
GustavoUT22 <gustavougartetorres@gmail.com>
2024-08-15 20:16:59 -0500

    __str__ and __init__ implemented

--- 02-intermediate/08-classes.py
+++ 02-intermediate/08-classes.py
@@ -3,15 +3,16 @@ class Animal:
     def __init__(self, nombre, peso):
         self.nombre = nombre
         self.peso = peso
+        print(self)
+
+    def __str__(self):
+        return f"{self.nombre} - {self.peso}"
 
     def comer(self):
         print(f"{self.nombre} está comiendo ")
 
-    def dormir():
-        print("El animal está durmiendo")
 
-
-gato = Animal("michi", "5kg")
-print(gato.nombre)
-print(gato.peso)
-gato.comer()
+gato = Animal("michi", "10kg")  # Creamos instancia u objeto de la clase
+print(gato)
+# print(str(gato))
+# gato.comer()
